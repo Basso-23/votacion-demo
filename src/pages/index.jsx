be4e963f306fb0fbcb4bd68d1024f8a3796264d4
Carlos Baso <110511071+Basso-23@users.noreply.github.com>
2024-05-03 01:47:12 -0500

    login terminado, revisado y documentado

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -20,7 +20,7 @@ const App = () => {
         if (router.pathname === "/") {
           router.push("/Dashboard");
         }
-        //* Asigna el email  del usuario
+        //* Asigna el email del usuario
         setCurrentUser(user.email);
       }
       //* Usuario NO logged
@@ -33,36 +33,36 @@ const App = () => {
     return () => validateUser();
   }, [router, setCurrentUser]);
 
+  //FUNCTION: Variables del form (react-hook-form)
   const { register, handleSubmit } = useForm();
 
+  //FUNCTION: onSubmit del form (react-hook-form)
   const onSubmit = async (data) => {
-    //* Muestra el loader
+    //* Loading true
     setLoading(true);
     try {
+      //* Enviando datos
       const userCredential = await signInWithEmailAndPassword(
         auth,
         data.email,
         data.password
       );
+      //* Informacion del usuario
       const user = userCredential.user;
-
-      //* Asigna el email del usuario a la variable userState
+      //* Asigna el email del usuario
       setCurrentUser(user.email);
-      //* Quita el mensaje de error
-      setError(false);
       //* Redirige a Dashboard
       router.push("/Dashboard");
     } catch (error) {
       setTimeout(() => {
-        //* Muestra el mensaje de error
+        //* Error true
         setError(true);
-        //* Quita el loader
+        //* Loading false
         setLoading(false);
+        //* Limpia los datos de los inputs
         document.getElementById("email").value = "";
         document.getElementById("password").value = "";
       }, 500);
-
-      console.log("Error al iniciar sesión:", error);
     }
   };
 
@@ -70,9 +70,13 @@ const App = () => {
     <main>
       <section className="pageSize flex min-h-[100svh]">
         <div className="mx-auto mt-36 h-fit w-full max-w-[400px] animate-fade-down animate-duration-[900ms]">
+          {/*//* Titulo */}
           <h1 className=" font-semibold mb-4 text-[25px]">Inicia sesión</h1>
+          {/*//* Form */}
           <form onSubmit={handleSubmit(onSubmit)}>
+            {/*//* Correo label */}
             <label>correo electrónico</label>
+            {/*//* Correo input */}
             <input
               id="email"
               required
@@ -80,7 +84,9 @@ const App = () => {
               placeholder=""
               {...register("email", { required: true })}
             />
+            {/*//* Contraseña label */}
             <label>contraseña</label>
+            {/*//* Contraseña input */}
             <input
               id="password"
               required
@@ -88,7 +94,7 @@ const App = () => {
               placeholder=""
               {...register("password", { required: true })}
             />
-
+            {/*//* Submit button */}
             {!loading ? (
               <input className="submit_btn" value="continuar" type="submit" />
             ) : (
@@ -103,6 +109,7 @@ const App = () => {
                 </div>
               </div>
             )}
+            {/*//* Error text*/}
             {error && (
               <div className="error text-[12px] font-medium">
                 Correo electrónico o contraseña incorrecta. Vuelve a intentarlo.
