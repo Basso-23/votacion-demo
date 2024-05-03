e2ba97b8b2fe9e73d6548cf701914b2698401194
Carlos Baso <110511071+Basso-23@users.noreply.github.com>
2024-05-02 22:54:27 -0500

    form sirve falta estilizar

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,14 +1,41 @@
 import React, { useState, useEffect } from "react";
 import { useAtom } from "jotai";
 import { userAtom } from "@/atom";
+import { useForm } from "react-hook-form";
 
 const App = () => {
   const [currentUser, setCurrentUser] = useAtom(userAtom);
 
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = (data) => console.log(data);
+
+  console.log(watch("example")); // watch input value by passing the name of it
+
   return (
     <main>
-      <div>dadadad</div>
-      <div>{currentUser}</div>
+      <section className="pageSize flex min-h-screen">
+        <div className="m-auto h-fit">
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <input
+              required
+              placeholder="nombre"
+              {...register("nombre", { required: true })}
+            />
+            <input
+              required
+              placeholder="edad"
+              {...register("edad", { required: true })}
+            />
+            <input type="submit" />
+          </form>
+        </div>
+      </section>
     </main>
   );
 };
